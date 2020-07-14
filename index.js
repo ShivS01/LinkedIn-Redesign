8637b6c2dad0df32e6b766c189857d3613bcced9
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-07-14 23:43:38 +0530

    added on-click toggle for side-nav

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const sideMenu = document.getElementsByClassName("User-mob");
-const nav = document.getElementsByClassName("side-nav");
-const overlay = document.getElementsByClassName("overlay");
+const sideMenu = document.querySelector(".User-mob");
+const nav = document.querySelector(".side-nav");
+const overlay = document.querySelector(".overlay");
 
 sideMenu.addEventListener("click", () => {
   overlay.classList.add("show");
   nav.classList.add("show-nav");
-  overlay.classList.remove("hide");
-
-  nav.classList.remove("hide-nav");
 });
 
 overlay.addEventListener("click", () => {
-  nav.classList.add("hide");
-  overlay.classList.add("hide");
-  nav.classList.remove("show");
-  overlay.classList.remove("show-overlay");
+  nav.classList.remove("show-nav");
+  overlay.classList.remove("show");
 });
