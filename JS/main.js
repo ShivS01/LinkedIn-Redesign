b4e859aacc357c1598f236f43f939b5c22f1ca45
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-07-15 23:14:28 +0530

    added side-nav

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,26 @@
 var elem = document.getElementsByClassName("see-more");
-for(var i = 0; i < elem.length; i++){
-    elem[i].onclick = function(){
-        if(this.innerHTML === "see less"){
-            this.previousSibling.style.display = "none";
-            this.innerHTML = "... see more";
-        }
-        else{
-            this.previousSibling.style.display = "inline";
-            this.innerHTML = "see less";
-        }
+for (var i = 0; i < elem.length; i++) {
+  elem[i].onclick = function () {
+    if (this.innerHTML === "see less") {
+      this.previousSibling.style.display = "none";
+      this.innerHTML = "... see more";
+    } else {
+      this.previousSibling.style.display = "inline";
+      this.innerHTML = "see less";
     }
+  };
 }
+
+const sideMenu = document.getElementById("ham-menu");
+const nav = document.querySelector(".side-nav");
+const overlay = document.querySelector(".overlay");
+
+sideMenu.addEventListener("click", () => {
+  overlay.classList.add("show");
+  nav.classList.add("show");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("show");
+  overlay.classList.remove("show");
+});
